b504c296075ef5c38690bea756fd088894090c19
gimkt <gimsongseob886@gmail.com>
2025-09-01 06:51:17 +0900

    initial commit

---
+++ screens/StartGameScreen.js
@@ -0,0 +1,12 @@
+import { View, Text ,TextInput} from 'react-native';
+
+function StartGameScreen() {
+    return (
+        <View>
+            <TextInput placeholder='Enter a number' />
+            <Text>Start Game Screen</Text>
+        </View>
+    )
+}
+
+export default StartGameScreen;
